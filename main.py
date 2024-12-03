c9167a4ecd595f68c6060822195bfd7a7b0c812e
Ivan Seredkin <ivan@netography.com>
2024-12-03 11:15:46 -0500

    #17 Code refactoring: move log funcs to its own file

--- main.py
+++ main.py
@@ -1,54 +1,17 @@
 import robin_stocks.robinhood as rh
 from openai import OpenAI
-from datetime import datetime
 import time
 import pandas as pd
 import json
 import re
 from pytz import timezone
 from config import *
+from log import *
 
 
 # Initialize session and login
 openai_client = OpenAI(api_key=OPENAI_API_KEY)
 rh.login(ROBINHOOD_USERNAME, ROBINHOOD_PASSWORD)
-
-
-# Print log message
-def log(level, msg):
-    log_levels = {"DEBUG": 1, "INFO": 2, "WARNING": 3, "ERROR": 4}
-    level_color_codes = {
-        "DEBUG": "\033[94m",
-        "INFO": "\033[92m",
-        "WARNING": "\033[93m",
-        "ERROR": "\033[91m"
-    }
-    timestamp_color_code = "\033[96m"
-    reset_color_code = "\033[0m"
-    if log_levels.get(level, 2) >= log_levels.get(LOG_LEVEL, 2):
-        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        level_space = " " * (8 - len(level))
-        print(f"{timestamp_color_code}[{timestamp}] {level_color_codes[level]}[{level}]{reset_color_code}{level_space}{msg}")
-
-
-# Print debug log message
-def log_debug(msg):
-    log("DEBUG", msg)
-
-
-# Print info log message
-def log_info(msg):
-    log("INFO", msg)
-
-
-# Print warning log message
-def log_warning(msg):
-    log("WARNING", msg)
-
-
-# Print error log message
-def log_error(msg):
-    log("ERROR", msg)
 
 
 # Run a Robinhood function with retries and delay between attempts (to handle rate limits)

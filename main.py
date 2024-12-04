18615729c8bb2bec011feec80295930f33f30eca
Ivan Seredkin <ivan@netography.com>
2024-12-04 10:40:52 -0500

    #29 Fix "SyntaxError: f-string: expecting '}'"

--- main.py
+++ main.py
@@ -334,9 +334,9 @@ def main():
                 sold_stocks = [f"{result['symbol']} ({result['quantity']})" for result in trading_results.values() if result['decision'] == "sell" and result['result'] == "success"]
                 bought_stocks = [f"{result['symbol']} ({result['quantity']})" for result in trading_results.values() if result['decision'] == "buy" and result['result'] == "success"]
                 errors = [f"{result['symbol']} ({result['details']})" for result in trading_results.values() if result['result'] == "error"]
-                log_info(f"Sold: {"None" if len(sold_stocks) == 0 else ', '.join(sold_stocks)}")
-                log_info(f"Bought: {"None" if len(bought_stocks) == 0 else ', '.join(bought_stocks)}")
-                log_info(f"Errors: {"None" if len(errors) == 0 else ', '.join(errors)}")
+                log_info(f"Sold: {'None' if len(sold_stocks) == 0 else ', '.join(sold_stocks)}")
+                log_info(f"Bought: {'None' if len(bought_stocks) == 0 else ', '.join(bought_stocks)}")
+                log_info(f"Errors: {'None' if len(errors) == 0 else ', '.join(errors)}")
             else:
                 run_interval_seconds = 60
                 log_info("Market is closed, waiting for next run...")
